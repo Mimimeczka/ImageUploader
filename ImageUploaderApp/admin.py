1ca4bed1512e01ad1dba4006e1770b58cd743f3c
Mimimeczka <justyna.zaczyk@onet.pl>
2021-05-14 19:35:32 +0200

    create models

--- ImageUploaderApp/admin.py
+++ ImageUploaderApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Image, Settings, AccountType
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Image)
+admin.site.register(Settings)
+admin.site.register(AccountType)
--- ImageUploaderApp/models.py
+++ ImageUploaderApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class AccountType(models.Model):
+    TYPES = {
+        (1, 'basic'),
+        (2, 'premium'),
+        (3, 'enterprise')
+    }
+    types = models.IntegerField(default=0, choices=TYPES)
+
+    def __str__(self):
+        return f'{self.types}'
+
+
+class User(models.Model):
+    mail = models.CharField(unique=True, null=False, max_length=50)
+    account_type = models.OneToOneField(AccountType, on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return f'{self.mail} ({self.account_type})'
+
+
+class Image(models.Model):
+    user_id = models.IntegerField(null=False)
+    photo = models.ImageField(null=False, blank=True)
+
+
+class Settings(models.Model):
+    account_type = models.OneToOneField(AccountType, on_delete=models.CASCADE, null=False)
+    resolution = models.IntegerField(null=False)
